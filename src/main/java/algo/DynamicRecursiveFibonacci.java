38d27b748ee11c4d606c5d38132a01de0335d6e7
is76 <is76@sunlife.com>
2019-07-30 17:19:49 +0800

    add algo

--- src/main/java/algo/DynamicRecursiveFibonacci.java
+++ src/main/java/algo/DynamicRecursiveFibonacci.java
@@ -3,24 +3,32 @@ package algo;
 import java.time.Duration;
 import java.time.Instant;
 
-public class DynamicFibonacci {
+public class DynamicRecursiveFibonacci {
 
   // Dynamic Programming - remember the past result and uses them to find new results
   // Uses tabulation such as arrays
 
+  static int fibNumbers[];
+
   public static int fibonacci(int n) {
-    int fibNumbers[] = new int[n + 2];
-    fibNumbers[0] = 0;
-    fibNumbers[1] = 1;
-    for (int i = 2; i <= n; i++) {
-      fibNumbers[i] = fibNumbers[i - 2] + fibNumbers[i - 1];
+    if (n < 2)
+      return n;
+    else if (fibNumbers[n] != 0)
+      return fibNumbers[n];
+    else {
+      int val = fibonacci(n - 2) + fibonacci(n - 1);
+      fibNumbers[n] = val;
+      return val;
     }
-    return fibNumbers[n];
+
   }
 
   public static void main(String[] args) {
+    int n = 40;
+    fibNumbers = new int[n + 1];
+
     Instant start = Instant.now();
-    System.out.println(fibonacci(40));
+    System.out.println(fibonacci(n));
     Instant end = Instant.now();
     long timeElapsed = Duration.between(start, end).toMillis();
     System.out.println("elapsed time in ms ::: " + timeElapsed);
